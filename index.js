a46be38ecae74231256b32002605dd286e118d8d
Suraj Ahmed <suraj@geekyants.com>
2021-09-09 15:41:33 +0530

    Changed command for production

--- index.js
+++ index.js
@@ -126,7 +126,13 @@ async function updateFiles(srcPath) {
       transformerPath = path.join(__dirname, "transformer.js");
     }
     const transformCommand =
-      runCommand + " " + options + " -t " + transformerPath + " " + srcPath;
+      runCommandProduction +
+      " " +
+      options +
+      " -t " +
+      transformerPath +
+      " " +
+      srcPath;
 
     // console.log(transformCommand, experimentalInlineProps, "hello transform");
     exec(transformCommand, { silent: silent }, (err, stdout) => {
--- test/app.js
+++ test/app.js
@@ -1,55 +1,8 @@
-import { extendTheme } from "native-base";
-
 import { NativeBaseProvider } from "native-base";
-
-const theme_v3 = extendTheme({
-  "lineHeights": {
-    "3": "12px",
-    "4": "16px",
-    "5": "20px",
-    "6": "24px",
-    "7": "28px",
-    "8": "32px",
-    "9": "36px",
-    "10": "40px",
-    "none": 1,
-    "shorter": 1.25,
-    "short": 1.375,
-    "base": 1.5,
-    "tall": 1.625,
-    "taller": 2
-  },
-  "letterSpacings": {
-    "xxs": -1.5,
-    "xs": -0.5,
-    "sm": 0,
-    "md": 0.1,
-    "lg": 0.15,
-    "xl": 0.25,
-    "2xl": 0.4,
-    "3xl": 0.5,
-    "4xl": 1.25,
-    "5xl": 1.5
-  },
-  "fontSizes": {
-    "xxs": 10
-  },
-  "radii": {
-    "sm": 2,
-    "md": 4,
-    "lg": 6,
-    "xl": 8,
-    "pill": 25
-  },
-  "borderWidth": {
-    "none": 0
-  }
-});
-      
 
 function App() {
   return (
-    (<NativeBaseProvider theme={theme_v3}>
+    <NativeBaseProvider>
       <Box bg="primary.500" p={4} />
       <Box
         lineHeight="19"
@@ -61,7 +14,7 @@ function App() {
         Hello World
       </Box>
       <Box
-        lineHeight="19"
+        lineHeight="19px"
         letterSpacing="2xs"
         radius="xs"
         fontSizes="23px"
@@ -69,10 +22,7 @@ function App() {
       >
         Hello World
       </Box>
-      <Box letterSpacing="2xs" radius="xs" fontSizes="23px" color="red.200">
-        Hello World
-      </Box>
-    </NativeBaseProvider>)
+    </NativeBaseProvider>
   );
 }
 
